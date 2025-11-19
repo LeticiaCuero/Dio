28258a5b13556e2e500d580950262d6e6235f7a2
LeticiaCuero <leticia.cuero06@gmail.com>
2025-11-19 15:36:28 -0300

    update

--- src/Santander 2025/A1.js
+++ src/Santander 2025/A1.js
@@ -1,9 +1,6 @@
 console.log("Digite o nome do seu jogador: ")
 let nickname = "Saori"
 console.log("Bem vindo " + nickname)
-
 const notify = "Pokemon GO diz: "
-
 console.log(notify + "Tem um novo evento chegando!")
 console.log(notify + "Prepare-se para capturar pokemons raros!")
-
