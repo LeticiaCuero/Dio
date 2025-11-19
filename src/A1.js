6e08655b3d7d2fdd54e54924f1a571224326f2db
LeticiaCuero <leticia.cuero06@gmail.com>
2025-11-19 15:02:30 -0300

    update

--- src/A1.js
+++ src/A1.js
@@ -1,3 +1,9 @@
 console.log("Digite o nome do seu jogador: ")
-let nickname = "Leticia"
+let nickname = "Saori"
 console.log("Bem vindo " + nickname)
+
+const notify = "Pokemon GO diz: "
+
+console.log(notify + "Tem um novo evento chegando!")
+console.log(notify + "Prepare-se para capturar pokemons raros!")
+
