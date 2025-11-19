2f187d5037b16b385fdef38e9c36296c4dc80055
LeticiaCuero <leticia.cuero06@gmail.com>
2025-11-19 17:40:14 -0300

    update

---
+++ src/Santander 2025/Matriz.js
@@ -0,0 +1,8 @@
+let pokemon = ["Pikachu", "Bulbasaur", "Charmander"]
+let time = [
+    ["pikachu", "M", "level 1"],
+    ["bulbasaur", "F", "level 5"],
+    ["charmander", "M", "level 8"]
+]
+
+console.log("O " + pokemon[0] + " é do sexo " + time[0][1])
---
+++ src/Santander 2025/Vetores.js
@@ -0,0 +1,4 @@
+let pokemon = ["Pikachu", "Bulbasaur", "Charmander"]
+//pokemon.pop()
+//pokemon.shift()
+console.log(pokemon[1])
